a11eeec79c33d14723b0567b276e5fd3744668a3
BramV-2158806 <99867493+BramV-2158806@users.noreply.github.com>
2025-10-31 11:41:43 +0100

    Refactored slicing plane

---
+++ briocheSlicer/Slicing/SlicingPlane.cs
@@ -0,0 +1,67 @@
+﻿using HelixToolkit.Wpf;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using System.Windows.Media.Media3D;
+
+namespace briocheSlicer.Slicing
+{
+    internal class SlicingPlane
+    {
+        private Point3D planeCenter;
+        private Vector3D planeNormal;
+        private int planeSize;
+
+        private GeometryModel3D model;
+
+        public SlicingPlane(Point3D center, int size = 100)
+        {
+            planeCenter = center;
+            planeNormal = new Vector3D(0, 0, 1);
+            planeSize = size;
+
+            model = Update_Model();
+        }
+
+        private GeometryModel3D Update_Model()
+        {
+            // Create plane
+            var plane = new Plane3D(planeCenter, planeNormal);
+
+            // Create visual representation of the plane
+            var rect = new RectangleVisual3D
+            {
+                Origin = plane.Position,
+                Normal = plane.Normal,
+                LengthDirection = new Vector3D(1, 0, 0),
+                Width = planeSize,
+                Length = planeSize,
+                Fill = Brushes.OrangeRed,
+                Material = MaterialHelper.CreateMaterial(Brushes.OrangeRed, 0.5, 100, true)
+            };
+
+            // Safe for later
+            model = rect.Model;
+            return rect.Model;
+        }
+
+        public GeometryModel3D Get_Model()
+        {
+            return model;
+        }
+
+        /// <summary>
+        /// Updates the plane's center position and refreshes the model.
+        /// </summary>
+        /// <param name="newCenter"></param>
+        public void Set_Center(Point3D newCenter)
+        {
+            planeCenter = newCenter;
+            Update_Model();
+
+        }
+    }
+}
--- briocheSlicer/Workers/TheSlicer.cs
+++ briocheSlicer/Workers/TheSlicer.cs
@@ -6,42 +6,28 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Media;
 using System.Windows.Media.Media3D;
+using briocheSlicer.Slicing;
 
 namespace briocheSlicer.Workers
 {
     internal class TheSlicer
     {
-        private Vector3D slicingPlaneNormal;
+        private SlicingPlane? slicingPlane;
 
         public TheSlicer()
         {
-            slicingPlaneNormal = new Vector3D(0, 0, 1);
         }
 
         public GeometryModel3D Create_Slicing_plane(Rect3D modelBounds)
         {
-            // Point and normal define plane
+            // Plane should be centered on the model center.
             var modelCenter = new Point3D(
                 modelBounds.X + modelBounds.SizeX / 2,
                 modelBounds.Y + modelBounds.SizeY / 2,
                 modelBounds.Z + modelBounds.SizeZ / 2);
-            var pointOnPlane = modelCenter;
 
-            // Define plane
-            var plane = new Plane3D(pointOnPlane, slicingPlaneNormal);
-
-            // Create visual representation of the plane
-            var rect = new RectangleVisual3D
-            {
-                Origin = plane.Position,
-                Normal = plane.Normal,
-                LengthDirection = new Vector3D(1, 0, 0),
-                Width = 100,
-                Length = 100,
-                Fill = Brushes.OrangeRed,
-                Material = MaterialHelper.CreateMaterial(Brushes.OrangeRed, 0.5, 100, true)
-            };
-            return rect.Model;
+            slicingPlane = new SlicingPlane(modelCenter);
+            return slicingPlane.Get_Model();
         }
     }
 }
